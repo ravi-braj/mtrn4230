7b821ac58cd7f66a429fe0530f1c7beeeeec2a56
ravi-braj <fastbucks79@gmail.com>
2017-08-15 17:51:29 +1000

    Added ClientMain.m and put some stub structure in

---
+++ ClientMain.m
@@ -0,0 +1,27 @@
+%% Client main
+%% 15 August 2017
+%% User interface for ABB robot
+
+
+%%%%%%%%%%%%% 1 SETUP %%%%%%%%%%%%%%
+
+%----- GLOBAL FLAGS ----
+
+global exit;
+
+
+%%%%%%%%%%%%% 2 INITIALISATIONS %%%%%%%%%%%%
+
+
+
+
+%%%%%%%%%%%%% 3 MAIN LOOP %%%%%%%%%%%%%%%
+
+while (~exit)
+    
+    
+end
+
+
+
+%%%%%%%%%%%%% 4 CLEAN UP %%%%%%%%%%%%
