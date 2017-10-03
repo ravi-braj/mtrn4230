cc71ac0200b9aeb68c8cdc6632eefe80271421fd
danielcastillo78310 <danielcastillo78310@gmail.com>
2017-10-03 16:21:43 +1100

    UpdateGame ReadyToTest + Conveyor Blocks some issues

--- Vision/detectConveyorBlocks/detectConveyorBlocks.m
+++ Vision/detectConveyorBlocks/detectConveyorBlocks.m
@@ -1,18 +1,16 @@
-function [blocks, box] = detectConveyorBlocks()%I)
+function [blocks, box] = detectConveyorBlocks(im)
     % This function takes in a 1200 x 1600 image and detects a set of 
     % blocks which reside within a carry box. It returns blocks which is 
     % structured [x, y]. This allows the robot arm to swap blocks.
     
-    I = imread('img1.jpg');
-    
     % Erase anything outside conveyor region of interest (Conveyor belt)
-    I(:,1:556,:) = 0;
-    I(:,1137:end,:) = 0;
-    I(705:end,:,:) = 0;
-    figure(1); imshow(I);
+    im(:,1:556,:) = 0;
+    im(:,1137:end,:) = 0;
+    im(705:end,:,:) = 0;
+    figure(1); imshow(im);
     
     % Generate conveyor mask (inverse should leave the box)
-    Conveyor_BW = conveyorMask(I);
+    Conveyor_BW = conveyorMask(im);
     conv_stats = regionprops('table',~Conveyor_BW,'Area','FilledImage','BoundingBox');
     
     % Cleanup Conveyor_BW using filled image and produce BoxMask
@@ -20,13 +18,13 @@ function [blocks, box] = detectConveyorBlocks()%I)
     s = conv_stats(NoiseIdx,:);
     sz = size(s.FilledImage,1);
     
-    BoxMask = false(size(I,1),size(I,2));
+    BoxMask = false(size(im,1),size(im,2));
     for n = 1:sz
         x0 = floor(s.BoundingBox(n,2));
         y0 = floor(s.BoundingBox(n,1));
         ix = 0:size(s.FilledImage{n},1)-1;
         iy = 0:size(s.FilledImage{n},2)-1;
-        BoxMask(x0+ix,y0+iy) = s.FilledImage{n}; 
+        BoxMask(x0:(x0+ix),y0:(y0+iy)) = s.FilledImage{n}; 
     end
     
     % Final set of erodes for good measure (noise removal)
@@ -50,25 +48,25 @@ function [blocks, box] = detectConveyorBlocks()%I)
         box.orient = rad2deg(atan2(y,x));
 
         % Apply BoxMask to Image (leaves only the box)
-        I(~BoxMask(:,:,[1, 1, 1])) = 0;
+        im(~BoxMask(:,:,[1, 1, 1])) = 0;
 
         % Plot BoxMask
-        figure(1); imshow(I); hold on; plot(box.x,box.y,'*');
+        figure(1); imshow(im); hold on; plot(box.x,box.y,'*');
 
         % Generate colour/shape mask
-        BW_Red = ConvRedMask(I);
-        BW_Orange = ConvRedMask(I);
-        BW_Yellow = ConvYellowMask(I);
-        BW_Green = ConvGreenMask(I);
-        BW_Blue = ConvBlueMask(I);
-        BW_Purple = ConvPurpleMask(I);
+        BW_Red = ConvRedMask(im);
+        BW_Orange = ConvRedMask(im);
+        BW_Yellow = ConvYellowMask(im);
+        BW_Green = ConvGreenMask(im);
+        BW_Blue = ConvBlueMask(im);
+        BW_Purple = ConvPurpleMask(im);
         ColourMask = BW_Red | BW_Orange | BW_Yellow | BW_Green | BW_Blue | BW_Purple;
 
         % Apply colour/shape mask
-        I(~ColourMask(:,:,[1, 1, 1])) = 0;
+        im(~ColourMask(:,:,[1, 1, 1])) = 0;
 
         % Plot image with box mask 
-        figure(2); imshow(I);
+        figure(2); imshow(im);
 
         % Extract shape centers
         shape_stats = regionprops('table',ColourMask,'Area','Centroid');
--- Vision/updateGameState/updateGameState.m
+++ Vision/updateGameState/updateGameState.m
@@ -1,5 +1,38 @@
-function gameState = updateGameState()
+function [Player1,Player2] = UpdateGameState(im)
+    Origin1 = [423, 290];
+    Player1 = zeros(6,2);
     
-    gameState = [];
+    Origin2 = [1124, 290];
+    Player2 = zeros(6,2);
+
+    % Optional brightness/contrast adjustment
+    %im = imadjust(im, [0, 0, 0; 0.53, 0.48, 0.47], []);
     
+    % Load our neural nets
+    load('convnetShape.mat'); % For shapes
+    load('convnetColour.mat'); % For colours
+    
+    for i = 1:6
+        % Extract a player1 grid slot
+        rgbBlock1 = imcrop(im,[Origin1(1),Origin1(2)+(i-1)*55,49,49]);
+
+        % Classify each the grid slot
+        [colour, shape] = ClassifyBlock(rgbBlock1, ...
+            convnetColour, convnetShape);
+
+        % Update Board
+        Player1(i,1) = colour;
+        Player1(i,2) = shape;
+        
+        % Extract a player2 grid slot
+        rgbBlock2 = imcrop(im,[Origin2(1),Origin2(2)+(i-1)*55,49,49]);
+        
+        % Classify each the grid slot 
+        [colour, shape] = ClassifyBlock(rgbBlock2, ...
+            convnetColour, convnetShape);
+
+        % Update Board
+        Player2(i,1) = shape;
+        Player2(i,2) = colour;
+    end
 end
