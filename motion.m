2c4c1d88aee2c2083b0dd0ba006b395262743f61
ravi-braj <fastbucks79@gmail.com>
2017-11-03 13:47:33 +1100

    made it so pick and place work off robot coord system not off pixels -function names still the same though

--- motion.m
+++ motion.m
@@ -31,19 +31,34 @@ classdef motion < handle
     end
     methods
         function obj = motion()
+            %ptablehome: centre point of robot
+            %[175,0,147]
             %define board positions
-            obj.board_topLeft = [551, 286];
-            obj.board_bottomRight = [1042, 785];
+            %width: 35mm +1mm borders
+            %obj.board_topLeft = [551, 286];
+            %obj.board_bottomRight = [1042, 785];
             
-            obj.p1_topLeft = [418, 286];
-            obj.p2_topLeft = [1130, 286];
+            %obj.p1_topLeft = [418, 286];
+            %obj.p2_topLeft = [1130, 286];
             
-            obj.squareSize = 56;
+            %obj.squareSize = 56;
             
             %set orientation point
-            obj.orientationPoint = [1140, 709];
+            %obj.orientationPoint = [1140, 709];
             
             obj.boxLocation = [0, 0];
+            
+            obj.board_topLeft = [0+175, -162];
+            %obj.board_bottomRight = [1042, 785];
+            
+            obj.p1_topLeft = [0+175, -230];
+            obj.p2_topLeft = [0+175, 230];
+            
+            obj.squareSize = 36;
+            
+            %set orientation point
+            obj.orientationPoint = [288+175, 230];
+            
             
 
         end
@@ -192,8 +207,8 @@ classdef motion < handle
         %takes in the grid square on the board and returns a pixel
         %coordinate. Top left of board is (0, 0)
         function [x_p, y_p] = boardToPixel(obj, x, y)
-            x_p = obj.board_topLeft(1) + x*obj.squareSize + 0.5*obj.squareSize;
-            y_p = obj.board_topLeft(2) + y*obj.squareSize + 0.5*obj.squareSize;
+            x_p = obj.board_topLeft(1) + y*obj.squareSize + 0.5*obj.squareSize;
+            y_p = obj.board_topLeft(2) + x*obj.squareSize + 0.5*obj.squareSize;
         end
         
         %takes in a player number and square and returns a pixel
@@ -202,11 +217,11 @@ classdef motion < handle
             %alternatively, could use topcorner, bottomcorner and board
             %size to determine position of grid squares.
             if(playerID == 0)
-                y_p = obj.p1_topLeft(2) + (n-1)*obj.squareSize+0.5*obj.squareSize;
-                x_p = obj.p1_topLeft(1) + 0.5*obj.squareSize;
+                x_p = obj.p1_topLeft(2) + (n-1)*obj.squareSize+0.5*obj.squareSize;
+                y_p = obj.p1_topLeft(1) + 0.5*obj.squareSize;
             else
-                y_p = obj.p2_topLeft(2) + (n-1)*obj.squareSize+0.5*obj.squareSize;
-                x_p = obj.p2_topLeft(1) + 0.5*obj.squareSize;
+                x_p = obj.p2_topLeft(2) + (n-1)*obj.squareSize+0.5*obj.squareSize;
+                y_p = obj.p2_topLeft(1) + 0.5*obj.squareSize;
             end
         end
     end
@@ -221,48 +236,52 @@ function [rs_x, rs_y, rs_z] = convertCoordsConveyor(x, y)
 % hObject    handle to choosePoint_table (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global ui;
-
-
-    M = undistortPoints([x,y], ui.conveyorParams);
-    x = M(1,1);
-    y = M(1,2);
-    z = 10;
-    y =1200 - y;
-    if(x>1600) || (y>1200) || (x<0) || (y<0)
-        x = NaN;
-        y = NaN;
-        z = NaN;
-    end
-    
-    conveyorOffsetXPx = 215;
-    conveyorOffsetYPx = 683;
-    
-    pxToMM = 0.659375;
-    
-    rs_y = (x - conveyorOffsetXPx)*pxToMM;
-    rs_x = (-y + conveyorOffsetYPx)*pxToMM-16;
+%     global ui;
+% 
+% 
+%     M = undistortPoints([x,y], ui.conveyorParams);
+%     x = M(1,1);
+%     y = M(1,2);
+%     z = 10;
+%     y =1200 - y;
+%     if(x>1600) || (y>1200) || (x<0) || (y<0)
+%         x = NaN;
+%         y = NaN;
+%         z = NaN;
+%     end
+%     
+%     conveyorOffsetXPx = 215;
+%     conveyorOffsetYPx = 683;
+%     
+%     pxToMM = 0.659375;
+%     
+%     rs_y = (x - conveyorOffsetXPx)*pxToMM;
+%     rs_x = (-y + conveyorOffsetYPx)*pxToMM-16;
+    rs_x = x;
+    rs_y = y;
     rs_z = 33;
 end
 
 function [rs_x, rs_y, rs_z] = convertCoordsTable(x, y)
     global ui;
-    M = undistortPoints([x,y], ui.tableParams);
-    x = M(1,1);
-    y = M(1,2);
-    y =1200 - y;
-    if(x>1600) || (y>1200) || (x<0) || (y<0)
-        x = NaN;
-        y = NaN;
-        z = NaN;
-    end
-    
-    pxToMM = 0.659375;
-    
-    tableXoffsetPx = 800; 
-    tableYoffsetPx = 1178;
-    
-    rs_y = (x - tableXoffsetPx)*pxToMM;
-    rs_x = (-y + tableYoffsetPx)*pxToMM;    
+%     M = undistortPoints([x,y], ui.tableParams);
+%     x = M(1,1);
+%     y = M(1,2);
+%     y =1200 - y;
+%     if(x>1600) || (y>1200) || (x<0) || (y<0)
+%         x = NaN;
+%         y = NaN;
+%         z = NaN;
+%     end
+%     
+%     pxToMM = 0.659375;
+%     
+%     tableXoffsetPx = 800; 
+%     tableYoffsetPx = 1178;
+%     
+%     rs_y = (x - tableXoffsetPx)*pxToMM;
+%     rs_x = (-y + tableYoffsetPx)*pxToMM;    
+    rs_x = x;
+    rs_y = y;
     rs_z = 147+7;
 end
