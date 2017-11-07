bff850ed59bf0fc6d213f724922218bed0bc9a05
danielcastillo78310 <danielcastillo78310@gmail.com>
2017-11-07 18:02:36 +1100

    detectOnTable is accually perfect

--- Vision/detectOnTable/detectOnTable.m
+++ Vision/detectOnTable/detectOnTable.m
@@ -1,19 +1,24 @@
 function c = detectOnTable()
+
     close all; clear all;
     im = imread('orientation.jpg');
+    
+    % Load our neural nets
+    load('convnetShape.mat'); % For shapes
+    load('convnetColour.mat'); % For colours
     
     % Generate block mask
     Block_BW = createBlockMask(im);
     Block_BW(1:225,:) = 0;
-    figure(1); 
-    subplot(1,3,1);
-    imshow(Block_BW);
+%     figure(1); 
+%     subplot(1,3,1);
+%     imshow(Block_BW);
     se = strel('cube',3);
     Block_BW = imerode(Block_BW, se);
     se = strel('cube',6);
     Block_BW = imdilate(Block_BW, se);
-    subplot(1,3,2);
-    imshow(Block_BW);
+%     subplot(1,3,2);
+%     imshow(Block_BW);
     
     %Insert Filled Image and create BlockMask
     bw_stats = regionprops('table',Block_BW,'Area','BoundingBox','FilledImage');
@@ -36,8 +41,10 @@ function c = detectOnTable()
         BlockMask(row:(row+sz(1)-1),col:(col+sz(2)-1)) = bw_stats.FilledImage{idx(j)};
     end
     
-    subplot(1,3,3);
-    imshow(BlockMask);
+%     subplot(1,3,3);
+%     imshow(BlockMask);
+
+    im = imadjust(im, [0, 0, 0; 0.50, 0.48, 0.47], []);
     
     stats = regionprops('table',BlockMask,'Centroid','Area','PixelList');
     inds = find(stats.Area > 2500);
@@ -64,16 +71,16 @@ function c = detectOnTable()
         
         % Extract a player1 grid slot
         rgbBlock1 = ExtractBlockIm(im, c(k,1), c(k,2), c(k,3), 50);
-        figure(2);
-        imshow(rgbBlock1);
+%         figure(2);
+%         imshow(rgbBlock1);
 
         % Classify each the grid slot
         [colour, shape] = ClassifyBlock(rgbBlock1, ...
             convnetColour, convnetShape);
 
         % Update Board
-        c(i,4) = colour;
-        c(i,5) = shape;
+        c(k,4) = colour;
+        c(k,5) = shape;
     end
 end
 
@@ -81,14 +88,14 @@ function imExtracted = ExtractBlockIm(im, x, y, angle, sideLength)
 
     % Initial crop to bounding box of rotated square
     xmin = x - sideLength;
-    ymin = y - sideLength;
+    ymin = y + sideLength;
     xmax = x + sideLength;
-    ymax = y + sideLength;
-    rect = [ymin, xmin, ymax-ymin, xmax-xmin];
+    ymax = y - sideLength;
+    rect = [xmin, ymax, abs(xmax-xmin), abs(xmax-xmin)];
     imExtracted = imcrop(im, rect);
 
     % Rotate image by angle
-    imExtracted = imrotate(imExtracted, -rad2deg(angle), 'crop');
+    imExtracted = imrotate(imExtracted, angle, 'crop');
 
     % Crop again to square dimensions
     sz = [size(imExtracted, 1), size(imExtracted, 2)];
@@ -99,21 +106,3 @@ function imExtracted = ExtractBlockIm(im, x, y, angle, sideLength)
     imExtracted = imcrop(imExtracted, rect2);
 
 end
-
-%         [~, i1] = max(stats.PixelList{i}(:,1));
-%         [~, i2] = max(stats.PixelList{i}(:,2));
-%         [~, i3] = min(stats.PixelList{i}(:,1));
-%         [~, i4] = min(stats.PixelList{i}(:,2));
-%         
-%         x1_ = stats.PixelList{i}(i1,1) - stats.PixelList{i}(i2,1)
-%         y1_ = stats.PixelList{i}(i1,2) - stats.PixelList{i}(i2,2)
-%         x1_ = stats.PixelList{i}(i3,1) - stats.PixelList{i}(i4,1)
-%         y1_ = stats.PixelList{i}(i3,2) - stats.PixelList{i}(i4,2)
-%         
-%         hold on;
-%         plot(stats.PixelList{i}(i1,1),stats.PixelList{i}(i1,2),'*b');
-%         plot(stats.PixelList{i}(i2,1),stats.PixelList{i}(i2,2),'*r');
-%         plot(stats.PixelList{i}(i3,1),stats.PixelList{i}(i3,2),'*g');
-%         plot(stats.PixelList{i}(i4,1),stats.PixelList{i}(i4,2),'*m');
-%         
-%         c(k,3) = rad2deg((atan2(y1_,x1_)))
