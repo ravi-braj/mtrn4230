8b0a062590a228c75b92fa2bfc8b8299d3e6d7ac
danielcastillo78310 <danielcastillo78310@gmail.com>
2017-11-10 20:54:14 +1100

    extra random plot blocks removed

--- Vision/plotBlocks.m
+++ Vision/plotBlocks.m
@@ -1,25 +1,33 @@
 function plotBlocks()
-    global ui;
-
-    % ui.clientGUIData.camera_table
-    % ui.clientGUIData.camera_conveyor
     close all; clear all;
-    im = imread('con2.jpg');
     
-    %c = detectTable(im)
-    [c, box, FoundBox] = detectConveyor(im)
+    source = 1; % Table = 0; Conveyor = 1;
+    
+    if (source)
+        im = imread('conv2.jpg');
+        [c, box, FoundBox] = detectConveyor(im);
+    else
+        im = imread('orientation.jpg');
+        c = detectTable(im);
+    end
     
     figure(1); imshow(im);  hold on;
     n = size(c,1);
     
+    x = c(:,1); y = c(:,2); thi = deg2rad(c(:,3));
+    u = cos(thi);
+    v = sin(thi);
+    
     for i = 1:n
-        x = c(i,1); y = c(i,2); thi = deg2rad(c(i,3));
-        u = 30*cos(thi);
-        v = 30*sin(thi);
-        
-        plot(x,y,'*w');
-        quiver(x,y,u,v,'w','linewidth',1,'MaxHeadSize',50);
-        s = sprintf('[C: %d,S: %d,R: %d]',c(i,4),c(i,5),c(i,6));
-        text(x + 35,y,s,'HorizontalAlignment','left')
+        s{i} = sprintf('[C: %d,S: %d,R: %d]',c(i,4),c(i,5),c(i,6));
+    end
+    
+    text(x + 45,y,s,'HorizontalAlignment','left','Color','g');
+    plot(x,y,'*w');
+    
+    if source
+        quiver(x,y,u,v,0.35,'w','linewidth',1.2);
+    else
+        quiver(x,y,u,v,0.1,'w','linewidth',1.2);
     end
 end
