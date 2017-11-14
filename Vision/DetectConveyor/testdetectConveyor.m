439afdcf5603d3b312cb985692d33d517c7493a7
jaywala <jaywala@users.noreply.github.com>
2017-11-14 13:44:14 +1100

    fix

--- Vision/DetectConveyor/testdetectConveyor.m
+++ Vision/DetectConveyor/testdetectConveyor.m
@@ -1,10 +1,10 @@
-coveyorObj = webcam(1); % coveyor
+coveyorObj = webcam(2); % coveyor
 %Set Resolutions to the 1600x1200 if available
 coveyorObj.Resolution = '1600x1200';
 
 while(1)
     coveyorRGB = snapshot(coveyorObj);
-    figure(23);
+    figure(25);
     imshow(coveyorRGB);
     detectCoveyor(coveyorRGB);
     pause(10);
